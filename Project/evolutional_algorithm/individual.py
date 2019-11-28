5855a55d0401128c7fd11df8643fe01aa0043849
Arton <arton-10@o2.pl>
2019-11-28 20:09:56 +0100

    Individual class: constructor, create_child, mutate

--- Project/evolutional_algorithm/individual.py
+++ Project/evolutional_algorithm/individual.py
@@ -1,16 +1,33 @@
+from random import randint
+from statistics import mean
+
+
 class Individual:
-    def __init__(self):
-        self._solution = []
-        pass
+    CHILDREN_AMOUNT = 7
+    MAX_SCORE = 10
+    MIN_SCORE = 0
+    _MUTATIONS_RATIO = 2/10  # percentage of genes that get changed
+
+    def __init__(self, solution_from_parents=None):
+        if solution_from_parents is not None:
+            self._solution = solution_from_parents
+        else:
+            self._solution = [randint(self.MIN_SCORE, self.MAX_SCORE)
+                              for child in range(self.CHILDREN_AMOUNT)
+                              ]
 
     def evaluate_fitness(self):
         pass
 
     def create_child(self, other_individual):
-        pass
+        child_solution = [mean(first_gene, second_gene) for first_gene, second_gene in zip(self._solution, other_individual._solution)]
+        return Individual(child_solution)
 
     def mutate(self):
-        pass
+        solution_length = len(self._solution)
+        amount_of_mutations = self.MUTATIONS_RATIO*solution_length
+        for mutation in range(amount_of_mutations):
+            self._solution[randint(0, solution_length)] = randint(0, self._maximum_score)
 
     def get_information(self):
         text = ""
