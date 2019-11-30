8b52e40d2089e7b671c999a5f7ee55efcb5c3dd4
Arton <arton-10@o2.pl>
2019-11-30 21:46:35 +0100

    Chages

--- Project/evolutional_algorithm/individual.py
+++ Project/evolutional_algorithm/individual.py
@@ -1,5 +1,6 @@
 from random import randint
 from statistics import mean
+from math import ceil
 
 
 class Individual:
@@ -8,18 +9,28 @@ class Individual:
     def __init__(self, solution):
         self._solution = solution
 
-    def evaluate_fitness(self):
-        pass
+    def evaluate_fitness(self, received_points):
+        amount_of_cookies = sum(self._solution)
+        penalty = 0
+        for left_child, left_cookies, right_child, right_cookies in zip(received_points[:-1], self._solution[:-1], received_points[1:], self._solution[1:]):
+            if left_child < right_child and not left_cookies < right_cookies:
+                penalty += right_child - left_child + (left_cookies - right_cookies) + 1
+            else:
+                if left_child > right_child and not left_cookies > right_cookies:
+                    penalty += left_child - right_child + (right_cookies - left_cookies) + 1
+        return amount_of_cookies + penalty
 
     def create_child(self, other_individual):
-        child_solution = [mean(first_gene, second_gene) for first_gene, second_gene in zip(self._solution, other_individual._solution)]
+        if other_individual is None:
+            print("WTF")
+        child_solution = [ceil(mean((first_gene, second_gene))) for first_gene, second_gene in zip(self._solution, other_individual._solution)]
         return Individual(child_solution)
 
     def mutate(self, min_score, max_score):
         solution_length = len(self._solution)
-        amount_of_mutations = self.MUTATIONS_RATIO*solution_length
-        for mutation in range(amount_of_mutations):
-            self._solution[randint(0, solution_length)] = randint(min_score, max_score)
+        amount_of_mutations = self._MUTATIONS_RATIO*solution_length
+        for mutation in range(ceil(amount_of_mutations)):
+            self._solution[randint(0, solution_length-1)] = randint(min_score, max_score)
 
     def get_information(self):
         text = ""
--- Project/evolutional_algorithm/population.py
+++ Project/evolutional_algorithm/population.py
@@ -1,21 +1,22 @@
 from .individual import Individual
-from random import rand, randint
+from random import randint, random
 
 
 class Population:
-    _AMOUNT_OF_CANDIDATES = 12
-    _AMOUNT_OF_CHILDREN = 6
+    _AMOUNT_OF_CANDIDATES = 50
+    _AMOUNT_OF_CHILDREN = 10
     _AMOUNT_OF_GENES = 7
-    _MIN_GENE = 0
+    _MIN_GENE = 1
     _MAX_GENE = 10
 
     def __init__(self,
+                 problem,
                  candidates_amount=_AMOUNT_OF_CANDIDATES,
                  children_amount=_AMOUNT_OF_CHILDREN,
-                 gene_amount=_AMOUNT_OF_GENES,
                  minimum_gene=_MIN_GENE,
                  maximum_gene=_MAX_GENE,
                  ):
+        self._problem = problem
         self._candidates = []
         self._generation = 0
         self._children_amount = children_amount
@@ -23,65 +24,66 @@ class Population:
         self._maximum_gene = maximum_gene
         for i in range(candidates_amount):
             solution = [randint(minimum_gene, maximum_gene)
-                        for child in range(gene_amount)
+                        for child in problem
                         ]
             self._candidates.append(Individual(solution))
+        self._best_solution = max(self._candidates, key=lambda x: x.evaluate_fitness(self._problem))
 
-    def get_best(self):
-        best = self._candidates[0]
-        best_fitness = best.evaluate_fitness()
-        for individual in self._candidates[1:]:
-            individual_fitness = individual.evaluate_fitness()
-            if best_fitness < individual_fitness:
+    def _set_best(self):
+        best = self._best_solution
+        best_fitness = best.evaluate_fitness(self._problem)
+        for individual in self._candidates:
+            individual_fitness = individual.evaluate_fitness(self._problem)
+            if best_fitness > individual_fitness:
                 best = individual
                 best_fitness = individual_fitness
-        return best.get_information()
+        self._best_solution = best
 
-    def _get_probabilites_for_roulette_selection(self):
-        fitness_list = [individual.evaluate_fitness() for individual in self._candidates]
-        fitness_sum = sum(fitness_list)
-        probabilities = []
-        previous_probability = 0
-        for fitness in fitness_list:
-            probability = previous_probability + fitness/fitness_sum
-            probabilities.append(probability)
-            previous_probability = probability
-        return probabilities
+    def roulette_select_individual(self, candidates_with_fitness):
+        fitness_sum = sum(1/candidate[1] for candidate in candidates_with_fitness)
+        spin = random()
+        probability_sum = 0
+        for potential_parent, fitness in candidates_with_fitness:
+            probability = 1 / (fitness*fitness_sum)
+            probability_sum += probability
+            if probability_sum > spin:
+                return potential_parent
 
-    def _select_parents(self):
-        # needs changing
-        parents = []
-        probabilities = self._get_probabilites_for_roulette_selection()
-        for parent in range(2*self._children_amount):
-            spin = rand()
-            for potential_parent, bound in zip(self._candidates, probabilities)):
-                if spin < bound:
-                    parents.append(potential_parent)
-                    break
-        return parents
+    def _select_pair_of_parents(self):
+        candidates_with_fitness = [(individual, individual.evaluate_fitness(self._problem)) for individual in self._candidates]
+        first_parent = self.roulette_select_individual(candidates_with_fitness)
+        candidates_without_first_parent = [(individual, fitness) for individual, fitness in candidates_with_fitness if individual != first_parent]
+        del candidates_with_fitness
+        second_parent = self.roulette_select_individual(candidates_without_first_parent)
+        return first_parent, second_parent
 
-    def _create_children(self, parents):
+    def _create_children(self):
         children = []
-        for first_parent, second_parent in zip(parents[::2], parents[1::2]):
-            child = Individual.create_child(first_parent, second_parent)
+        # for child_index in range(self._children_amount):
+        first_parent, second_parent = self._select_pair_of_parents()
+        child = Individual.create_child(first_parent, second_parent)
+        if(random() < 0.2):
             child.mutate(self._minimum_gene, self._maximum_gene)
-            children.append(child)
+        children.append(child)
         return children
 
     def _update_population(self, children):
-        candidates_with_fitness = [individual, individual.evaluate_fitness() for individual in self._candidates]
+        candidates_with_fitness = [(individual, individual.evaluate_fitness(self._problem)) for individual in self._candidates]
         for individual in children:
-            fitness_list.append((individual, individual.evaluate_fitness()))
-        fitness_list.sort(key=lambda x: x[1])
-        self._candidates = list(map(lambda x: x[0], fitness_list[:len(self._candidates)]))
+            candidates_with_fitness.append((individual, individual.evaluate_fitness(self._problem)))
+        candidates_with_fitness.sort(key=lambda x: x[1])
+        self._candidates = list(map(lambda x: x[0], candidates_with_fitness[:len(self._candidates)]))
 
-    def evolve(self, amount_of_iterations):
+    def evolve(self, amount_of_iterations=1):
         for i in range(amount_of_iterations):
-            parents = self._select_parents()
-            children = self._create_children(parents)
+            children = self._create_children()
             self._update_population(children)
+            self._set_best()
+            self._generation += 1
 
     def print_information(self):
-        print(f"Generacja: {self._generation}\n")
-        for index, individual in zip(range(self._AMOUNT_OF_CANDIDATES), self._candidates):
-            print(f"{index+1}: {individual.get_information()} : {individual.evaluate_fitness()}\n")
+        print(f"Generacja: {self._generation}")
+        for index, individual in zip(range(self._AMOUNT_OF_CANDIDATES), sorted(self._candidates, key=lambda x: x.evaluate_fitness(self._problem))):
+            print(f"{index+1}: {individual.get_information()} : {individual.evaluate_fitness(self._problem)}")
+        # for index, individual in zip(range(self._AMOUNT_OF_CANDIDATES), self._candidates):
+            # print(f"{index+1}: {individual.get_information()} : {individual.evaluate_fitness(self._problem)}")
